386f39bdfbdcf2d95f357027dc95b0d7ec78aef9
Hisoka6602 <91233659+Hisoka6602@users.noreply.github.com>
2025-04-09 22:28:43 +0800

    20250409

---
+++ Wxck.AdminTemplate.Domain/Repositories/Logs/IRiskControlLogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Wxck.AdminTemplate.Domain.Entities.Logs;
+
+namespace Wxck.AdminTemplate.Domain.Repositories.Logs {
+
+    public interface IRiskControlLogRepository : IRepository<RiskControlLogInfoModel> {
+    }
+}
